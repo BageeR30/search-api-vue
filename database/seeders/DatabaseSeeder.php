386a38abd403da4959cea7a86f7563555ee375cd
BageeR30 <47611085+BageeR30@users.noreply.github.com>
2022-03-22 21:28:46 +0500

    house seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\House;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $csv_data = [
+            ['The Victoria', 374662, 4, 2, 2, 2],
+            ['The Xavier', 513268, 4, 2, 1, 2],
+            ['The Como', 454990, 4, 3, 2, 3],
+            ['The Aspen', 384356, 4, 2, 2, 2],
+            ['The Lucretia', 572002, 4, 3, 2, 2],
+            ['The Toorak', 521951, 5, 2, 1, 2],
+            ['The Skyscape', 263604, 3, 2, 2, 2],
+            ['The Clifton', 386103, 3, 2, 1, 1],
+            ['The Geneva', 390600, 4, 3, 2, 2]
+        ];
+
+        $map = ['name', 'price', 'bedrooms', 'bathrooms', 'storeys', 'garages'];
+
+        $data = [];
+        foreach ($csv_data as  $set) {
+            $data[] = array_combine($map, $set);
+        }
+
+        foreach ($data as $house) {
+            House::create($house);
+        }
+        
     }
 }
